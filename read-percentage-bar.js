8ddb5883824afcbdab6b56c35ce4b09b0fba884b
danielvieira7 <danieldiasvieira@gmail.com>
2020-07-06 17:04:55 -0300

    1st concrete version

--- read-percentage-bar.js
+++ read-percentage-bar.js
@@ -1,11 +1,17 @@
+let bar = document.createElement("div")
+bar.style.height = "4px"
+bar.style.width = "0"
+bar.style.backgroundColor = "#6633cc"
 bar.style.position = "fixed"
 bar.style.top = "0"
 bar.style.left = "0"
 bar.style.zIndex = "9999"
-
 document.body.append(bar)
 
+let postWrapClass = "#primary"
+
 document.addEventListener("scroll", () => {
+    const postWrap = document.querySelector(postWrapClass)
     const textHeight = postWrap.offsetHeight
     const pagePostiionY = window.pageYOffset
     const updateBar = pagePostiionY * 100 / textHeight
